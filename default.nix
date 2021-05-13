695efd9b76601ca839d2318455592fa931a20f4d
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2021-05-13 03:27:34 -0400

    remove nix shell

--- default.nix
+++ default.nix
@@ -2,22 +2,16 @@
 with pkgs;
 let deps = [ (texlive.combine { inherit (texlive) scheme-basic hyperref geometry; }) ];
 in
-  if lib.inNixShell
-  then
-    mkShell {
-      nativeBuildInputs = deps;
-    }
-  else
-    stdenv.mkDerivation {
-      name = "resume";
-      src = ./.;
-      buildInputs = deps;
-      buildPhase = ''
-        mkdir -p $out
-        HOME=./. pdflatex resume.tex
-        cp resume.pdf $out
-      '';
-      installPhase = ''
-        echo done
-      '';
-    }
+  stdenv.mkDerivation {
+    name = "resume";
+    src = ./.;
+    buildInputs = deps;
+    buildPhase = ''
+      mkdir -p $out
+      HOME=./. pdflatex resume.tex
+      cp resume.pdf $out
+    '';
+    installPhase = ''
+      echo done
+    '';
+  }
